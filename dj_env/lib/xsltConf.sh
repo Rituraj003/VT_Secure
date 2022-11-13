4b520e14c96bff24e7aca1315b5bddcfba2f084d
Rituraj <99554992+Rituraj003@users.noreply.github.com>
2022-11-12 20:12:56 -0500

    Added tensorflow enviroment

---
+++ dj_env/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/rituraj/Desktop/python_files/VT_Secure/dj_env/lib"
+XSLT_LIBS="-lxslt -L/Users/rituraj/Desktop/python_files/VT_Secure/dj_env/lib -lxml2 "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/rituraj/Desktop/python_files/VT_Secure/dj_env/include"
+MODULE_VERSION="xslt-1.1.37"
--- server/EncrySys/views.py
+++ server/EncrySys/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
 from django.views import generic
+import dataprofiler as dp
+import json
+import pandas as pd
+
 # Create your views here.
 
 class EncryptView(generic.TemplateView):
     template_name = "encrypt.html"
+    
+
+data = "HEllo this is Rituraj, you can contact me at rituraj@vt.edu"
+data = pd.Series([data])
+data_labeler = dp.DataLabeler(labeler_type = "unstructured")
+# predictions = data_labeler.predict(data)
+# print(predictions['pred'])
+# convert prediction to word format and ner format
+# Set the output to the NER format (start position, end position, label)
+# print("\n\n\n\n\n\n\n\n\n")
+
+data_labeler.set_params(
+    {'postprocessor': {'output_format':'ner', 'use_word_level_argmax':True}} 
+)
+predictions = data_labeler.predict(data)
+
+# display results
+print(predictions, "\n\n\n\n")
+print(predictions['pred'],"\n\n\n\n\n")
